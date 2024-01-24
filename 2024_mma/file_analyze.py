f218ee9418c00eee1b21ddfa53c3b425058ed310
[HyunsiU] <blackfire_@naver.com>
2024-01-24 15:58:02 +0900

    파일 hex 값 구분

--- 2024_mma/file_analyze.py
+++ 2024_mma/file_analyze.py
@@ -4,21 +4,25 @@ from Crypto.Cipher import AES
 
 class analyze:
     def __init__(self, file_path):
+        # 파일 path 및 추가 분석해야할 파일 
         self.file_path = file_path
         self.analyze_file = list()
 
     def unpack(self):
+        # app 파일 unpack
         print("application apktool unpack")
         shutil.copyfile(self.file_path, self.file_path+'.zip')
         command = 'java -jar apktool.jar d -s ' + self.file_path+'.zip' + " -o depack"
         os.system(command)
     
-    def app_tree(self, output=False):
+    def app_tree(self, folder,output=False):
+        # app의 file tree  구성 
         print('app tree analyze')
-        folder_name = 'depack'
+        folder_name = folder
         self.dir_file('./'+folder_name, 0, output)
 
     def dir_file(self, folder, count, output):
+        # 앱 트리 출력
         dirs = os.listdir(folder)
         if(output):
             print(' '*count+folder)
@@ -38,17 +42,24 @@ class analyze:
                 count-=1
 
     def aes_decrypt(self, file, decrpt_file, aes_key):
+        # aes 디코딩 및 dex파일의 확장자 확인
         print(file+' file decrypt')
         with open(file, 'rb') as f:
             data = f.read()
         
         cipher = AES.new(aes_key, AES.MODE_ECB)
         dec = cipher.decrypt(data)
-        print(file+" decrpt file to "+decrpt_file+" create")
-        with open('./depack/'+decrpt_file, 'wb') as f:
-           f.write(dec)
+        if(dec[0:3] == b'dex'):
+            print(file+" decrpt file to "+decrpt_file+" create")
+            with open('./depack/'+decrpt_file, 'wb') as f:
+                f.write(dec)
+        else:
+            print("dex 파일로 복호화 실패")
+        
+        
     
     def repack(self):
+        # depack 한 파일 repack.apk 생성
         print("repack.apk create")
         command = 'java -jar apktool.jar b depack -o repack.apk'
         os.system(command)
